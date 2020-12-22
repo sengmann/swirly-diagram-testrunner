3eeb5b688ed86acea67fe999d2b35ad90bdf8a64
Sascha Engmann <engmann@w11k.de>
2020-12-22 16:58:32 +0100

    correct import in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { TestMessage } from "@swirly/parser-rxjs/dist";
+import { TestMessage } from "@swirly/parser-rxjs";
 import { ColdObservable } from "rxjs/internal/testing/ColdObservable";
 
 export interface TestStream {
